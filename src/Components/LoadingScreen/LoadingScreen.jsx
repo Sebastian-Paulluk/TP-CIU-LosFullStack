726e0b525931838d5115b3739f69acd960047cf9
Sebastian-Paulluk <129323586+Sebastian-Paulluk@users.noreply.github.com>
2024-11-27 19:27:09 -0300

    modifico estilos de fabricantes

--- src/Components/LoadingScreen/LoadingScreen.jsx
+++ src/Components/LoadingScreen/LoadingScreen.jsx
@@ -4,8 +4,13 @@ import './LoadingScreen.scss'
 export const LoadingScreen =()=> {
     
     return (
-        <div className="d-flex justify-content-center loading-screen">
-            <Spinner animation="border" variant="secondary" role="status" className='spin' />
+        <div className="loading-screen">
+            <Spinner
+                animation="border"
+                variant="secondary"
+                role="status"
+                className='spin'
+            />
         </div>
     )
 }
